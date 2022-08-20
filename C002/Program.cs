2662153ec2e6b39e577034d9d0705b8e6ae5bedf
pavelyakovenko2012 <pavel.yackovencko2012@gmail.com>
2022-08-20 15:45:14 +0500

    002

---
+++ C002/Program.cs
@@ -0,0 +1,13 @@
+﻿/// start = 1 - начальная точка
+/// end - N - конец
+// [1;N]
+string PrintNumbers (int start, int end)
+{
+// Базовый случай
+if (start == end) return start.ToString();
+// Рекурсивный случай
+return (start + ", " + PrintNumbers(start+1, end));
+}
+
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(PrintNumbers(1, N));
