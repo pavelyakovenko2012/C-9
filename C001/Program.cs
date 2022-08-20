6e51f76e92ebb1693ac68a3e598f6cd9a4ad5bfe
pavelyakovenko2012 <pavel.yackovencko2012@gmail.com>
2022-08-20 15:44:33 +0500

    001

---
+++ C001/Program.cs
@@ -0,0 +1,12 @@
+﻿// Factorial
+// 4! = 4*3*2*1
+// number = 4 до 1
+int Factorial(int number)
+{
+if (number == 1) return 1;
+return number * Factorial(number - 1);
+}
+Console.WriteLine(Factorial(4));
+
+// Базовый случай: условие выхода из рекурсии
+// Рекурсивный случай: вызов функции из функции
