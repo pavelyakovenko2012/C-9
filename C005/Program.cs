435215a12837052b1eedf9de4b8c2878730a43a6
pavelyakovenko2012 <pavel.yackovencko2012@gmail.com>
2022-08-20 15:46:41 +0500

    005

---
+++ C005/Program.cs
@@ -0,0 +1,9 @@
+﻿int Power(int number, int degree)
+{
+// Базовый степень
+if (degree == 1) return number; // 5^1 = 5
+if (degree == 0) return 1;
+// Рекурсивный случай
+return (number * Power(number, degree - 1));
+}
+System.Console.WriteLine(Power(3,5));
