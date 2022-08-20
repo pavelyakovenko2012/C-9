165911c7362bdb00d9b1607d5e0b740ae6fc676f
pavelyakovenko2012 <pavel.yackovencko2012@gmail.com>
2022-08-20 15:46:12 +0500

    004

---
+++ C004/Program.cs
@@ -0,0 +1,10 @@
+﻿int SumNumbers(int number)
+{
+// Базовый случай
+if (number == 0) return 0;
+// number постоянно делится на 10 нацело
+// если число при делении на 10 дает 0 целых, то мы подсчитали
+// цифры
+return (number % 10 + SumNumbers(number / 10));
+}
+Console.WriteLine(SumNumbers(45));
